230387bedb6ecda3896d2a1e3f674f8189d91e2e
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-18 11:23:32 +0400

    custom graph and display the neighbours

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Graphs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Graphs");
+
+            mrGraph mrGraph = new mrGraph();
+            mrGraph.AddVertex("1");
+            mrGraph.AddVertex("2");
+            mrGraph.AddVertex("3");
+            mrGraph.AddVertex("4");
+
+            mrGraph.AddEdge("1", "2");
+            mrGraph.AddEdge("2", "3");
+            mrGraph.AddEdge("3", "4");
+            mrGraph.AddEdge("4", "1");
+
+            //displaying the neighbours
+            Dictionary<string,List<string>> keys = mrGraph.PrintGraph();
+            foreach(var vertex in keys)
+            {
+                Console.WriteLine("vertex: "+ vertex.Key);
+                Console.WriteLine("Neighbours: "+ string.Join(", ",vertex.Value));
+            }
+        }
+    }
+}
---
+++ mrGraph.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    internal class mrGraph
+    {
+        private Dictionary<string, List<string>> graph;
+
+        public mrGraph() 
+        {
+            graph = new Dictionary<string, List<string>>();
+        }
+        public void AddVertex(string vertex)
+        {
+            if(!graph.ContainsKey(vertex))
+            {
+                graph[vertex] = new List<string>();
+            }
+        }
+        public void AddEdge(string vertex1,string vertex2)
+        {
+            if(graph.ContainsKey(vertex1)&& graph.ContainsKey(vertex2)){
+                graph[vertex1].Add(vertex2);
+                graph[vertex2].Add(vertex1);
+            }
+        }
+        
+        // to display the neighbours
+        public Dictionary<string, List<string>> PrintGraph()
+        {
+            return graph;
+        }
+    }
+}
